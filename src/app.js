f7a01a009b7f5f4ce2468341e299b261aa9f0173
Vuyisanani <vmeteni@gmail.com>
2020-04-23 07:16:40 +0200

    Tests

--- src/app.js
+++ src/app.js
@@ -276,7 +276,15 @@ var Hand = /** @class */ (function () {
     };
     return Hand;
 }());
-var test = new Deck();
-test.shuffle();
-console.log(test.draw().name);
+// let test = new Deck();
+// test.shuffle();
+// console.log(test.draw().name);
+var h = new Hand([
+    new Card(3, Suit.Diamonds),
+    new Card(5, Suit.Diamonds),
+    new Card(6, Suit.Diamonds),
+]);
+console.log(h.has(5));
+console.log(h.has(6));
+console.log(h.has(7));
 //# sourceMappingURL=app.js.map
--- src/shuffletest.ts
+++ src/shuffletest.ts
@@ -1,5 +1,18 @@
-let test = new Deck();
+// let test = new Deck();
 
-test.shuffle();
+// test.shuffle();
 
-console.log(test.draw().name);
+// console.log(test.draw().name);
+
+let h = new Hand([
+    new Card(3, Suit.Diamonds),
+    new Card(5, Suit.Diamonds),
+    new Card(6, Suit.Diamonds),
+]);
+
+console.log(h.has(5));
+console.log(h.has(6));
+console.log(h.has(7));
+
+
+
